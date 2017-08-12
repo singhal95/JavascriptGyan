b79899a95c32c98d7c03dd96a7700a64682af919
nitin singhal <singhal95nitin@gmail.com>
2017-08-13 00:58:17 +0530

    Js overview is added

---
+++ app/src/main/java/singhal/a95/nitin/com/javascriptgyan/JavascriptOverview.java
@@ -0,0 +1,24 @@
+package singhal.a95.nitin.com.javascriptgyan;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class JavascriptOverview extends Fragment {
+
+
+    public JavascriptOverview() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_javascript_overview, container, false);
+    }
+}
